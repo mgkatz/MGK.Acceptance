bf6dc2a9931d1975992d08699b7d27a63b983d47
mkatz-zenfolio <mkatz@zenfolio.com>
2020-04-03 10:44:17 -0300

    Initial commit
    Initial commit

---
+++ src/MGK.Acceptance/Ensure.Parameter.cs
@@ -0,0 +1,118 @@
+﻿using MGK.Acceptance.Enums;
+using MGK.Extensions;
+using System;
+using System.Collections.Generic;
+
+namespace MGK.Acceptance
+{
+	/// <summary>
+	/// Ensures that values or objects complies with what is expected.
+	/// </summary>
+	public static partial class Ensure
+	{
+		/// <summary>
+		/// Ensures that parameters values complies with what is expected.
+		/// </summary>
+		public static class Parameter
+		{
+			/// <summary>
+			/// Ensure that a parameter is not null, otherwise throws an argument null exception.
+			/// </summary>
+			/// <param name="value">The parameter value.</param>
+			/// <param name="paramName">The parameter's name.</param>
+			public static void IsNotNull(object value, string paramName)
+				=> IsNotNull(value, paramName, Resources.AppMessages.ErrorParamNull);
+
+			/// <summary>
+			/// Ensure that a parameter is not null, otherwise throws an argument null exception with a specific message.
+			/// </summary>
+			/// <param name="value">The parameter value.</param>
+			/// <param name="paramName">The parameter's name.</param>
+			/// <param name="errorMessage">The specific error message.</param>
+			public static void IsNotNull(object value, string paramName, string errorMessage)
+				=> EvaluateParameter(value == null, paramName, errorMessage, ParameterErrorType.Null);
+
+			/// <summary>
+			/// Ensure that a parameter is not null nor empty, otherwise throws an argument exception.
+			/// </summary>
+			/// <param name="value">The parameter value.</param>
+			/// <param name="paramName">The parameter's name.</param>
+			public static void IsNotNullNorEmpty(string value, string paramName)
+				=> IsNotNullNorEmpty(value, paramName, Resources.AppMessages.ErrorParamNullNorEmpty);
+
+			/// <summary>
+			/// Ensure that a parameter is not null nor empty, otherwise throws an argument exception with a specific message.
+			/// </summary>
+			/// <param name="value">The parameter value.</param>
+			/// <param name="paramName">The parameter's name.</param>
+			/// <param name="errorMessage">The specific error message.</param>
+			public static void IsNotNullNorEmpty(string value, string paramName, string errorMessage)
+				=> EvaluateParameter(value.IsNullOrEmpty(), paramName, errorMessage);
+
+			/// <summary>
+			/// Ensure that a parameter is not null nor empty, otherwise throws an argument exception.
+			/// </summary>
+			/// <param name="value">The parameter value.</param>
+			/// <param name="paramName">The parameter's name.</param>
+			public static void IsNotNullNorEmpty<T>(IEnumerable<T> value, string paramName)
+				=> IsNotNullNorEmpty(value, paramName, Resources.AppMessages.ErrorParamNullNorEmpty);
+
+			/// <summary>
+			/// Ensure that a parameter is not null nor empty, otherwise throws an argument exception with a specific message.
+			/// </summary>
+			/// <param name="value">The parameter value.</param>
+			/// <param name="paramName">The parameter's name.</param>
+			/// <param name="errorMessage">The specific error message.</param>
+			public static void IsNotNullNorEmpty<T>(IEnumerable<T> value, string paramName, string errorMessage)
+				=> EvaluateParameter(value.IsNullOrEmpty(), paramName, errorMessage);
+
+			/// <summary>
+			/// Ensure that a parameter is not null nor empty nor only white spaces, otherwise throws an argument exception.
+			/// </summary>
+			/// <param name="value">The parameter value.</param>
+			/// <param name="paramName">The parameter's name.</param>
+			public static void IsNotNullNorEmptyNorWhiteSpace(string value, string paramName)
+				=> IsNotNullNorEmptyNorWhiteSpace(value, paramName, Resources.AppMessages.ErrorParamNullNorEmptyNorWhiteSpace);
+
+			/// <summary>
+			/// Ensure that a parameter is not null nor empty nor only white spaces, otherwise throws an argument exception with a specific message.
+			/// </summary>
+			/// <param name="value">The parameter value.</param>
+			/// <param name="paramName">The parameter's name.</param>
+			/// <param name="errorMessage">The specific error message.</param>
+			public static void IsNotNullNorEmptyNorWhiteSpace(string value, string paramName, string errorMessage)
+				=> EvaluateParameter(value.IsNullOrEmptyOrWhiteSpace(), paramName, errorMessage);
+
+			/// <summary>
+			/// Validate and evaluate a parameter and raise the proper exception if applicable.
+			/// </summary>
+			/// <param name="isValueObjectNotValid">True if the value of an object does not comply with the conditions.</param>
+			/// <param name="paramName">The name of the parameter that is being evaluated.</param>
+			/// <param name="errorMessage">The error message in case of an error.</param>
+			/// <param name="parameterErrorType">The type of the parameter error.</param>
+			private static void EvaluateParameter(
+				bool isValueObjectNotValid,
+				string paramName,
+				string errorMessage,
+				ParameterErrorType parameterErrorType = ParameterErrorType.Generic)
+			{
+				if (isValueObjectNotValid)
+				{
+					ValidateErrorMessage(errorMessage);
+					ValidateParamName(paramName);
+					Raise.Error.Parameter(paramName, errorMessage, parameterErrorType);
+				}
+			}
+
+			/// <summary>
+			/// Validates that the parameter name has a value.
+			/// </summary>
+			/// <param name="paramName">The parameter name.</param>
+			private static void ValidateParamName(string paramName)
+			{
+				if (paramName.IsNullOrEmptyOrWhiteSpace())
+					Raise.Error.Parameter(nameof(paramName), Resources.AppMessages.ErrorParamNameNotProvided);
+			}
+		}
+	}
+}
---
+++ test/MGK.Acceptance.Test/TestException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MGK.Acceptance.Test
+{
+	public class TestException : Exception
+	{
+		public TestException() : base()
+		{
+		}
+
+		public TestException(string message) : base(message)
+		{
+		}
+
+		public TestException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+	}
+}
